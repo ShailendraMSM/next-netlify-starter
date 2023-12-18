351e8a1520b7642ce8d10322fc81a222d7495728
Shailendra <69146504+ShailendraMSM@users.noreply.github.com>
2023-12-18 16:24:33 +0530

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -13,7 +13,7 @@ export default function Home() {
       <main>
         <Header title="Welcome to my app!" />
         <p className="description">
-          Get started by editing <code>index file</code>
+          Get started by editing index file
         </p>
       </main>
 
